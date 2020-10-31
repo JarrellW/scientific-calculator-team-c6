54a2f343f6203e8ec1990c29caa7f99fb8aca03d
kyle <kyle@zipcodes-MBP.fios-router.home>
2020-10-31 18:18:27 -0400

    created methods for trig and logs but they are not operational yet

--- src/main/java/com/zipcodewilmington/scientificcalculator/SciFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciFunctions.java
@@ -2,65 +2,72 @@ import com.zipcodewilmington.scientificcalculator.Console;
 
 public class SciFunctions {
 
+// These are not functional yet need to figure out the formatting
     public static void sin() {
         Integer i = Console.getIntegerInput("Enter an integer");
-        Console.println("%s ", Math.toRadians(Math.sin(i)));
+        Console.println("%s ", i, Math.toRadians(Math.sin(i)));
     }
-//    public static void cos() {
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Console.println("%s ", Math.toRadians(Math.cos(i)));
-//    }
-//    public static void tan() {
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Console.println("%s ", Math.toRadians(Math.tan(i)));
-//    }
-//    public static void sec() {
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Console.println("%s ", 1/Math.toRadians(Math.sin(i)));
-//    }
-//    public static void csc() {
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Console.println("%s ", 1/Math.toRadians(Math.cos(i)));
-//    }
-//    public static void cot() {
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Console.println("%s ", 1/Math.toRadians(Math.tan(i)));
+    public static void cos() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", i, Math.toRadians(Math.cos(i)));
+    }
+    public static void tan() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", i, Math.toRadians(Math.tan(i)));
+    }
+    public static void sec() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", i, 1/Math.toRadians(Math.sin(i)));
+    }
+    public static void csc() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", i, 1/Math.toRadians(Math.cos(i)));
+    }
+    public static void cot() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", i, 1/Math.toRadians(Math.tan(i)));
+    }
+    public static void log() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", Math.log10(i));
+    }
+    public static void naturalLog() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", Math.log(i));
+    }
+    public static void inverseLog() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", (Math.exp(Math.log10(i))));
+    }
+    public static void inverseNaturalLog() {
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Console.println("%s ", (Math.exp(Math.log(i))));
+    }
+    // Mode switching not sure how to use this yet understand changing the string formatting
+//      public static void binary() {
+//    ????
+//        }
+//      public static void octal() {
+//    %o
+//        }
+//      public static void decimal() {
+//    %d
+//        }
+//      public static void hexadecimal() {
+//    %h
+//        }
+    // Again not sure with how to go about mode changes that change the calculator and how it returns values
+
+//      public unitSwitch() {
+//        case radian Math.toRadians(num)
+//        case degree Math.toDegree(num)
 //    }
 
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Console.println("%s ", 1/Math.toRadians(Math.tan(i)));
+//   public memory() {
+//        case input  M+ adds the current displayed value in the memory and updates display
+//        case input  MC reset the memory
+//        case MRC recall the current value from memory to the display
 //    }
-//
-//
-////
-////
-////    public static void binary() {
-//////     /*
-//////        int bi = Integer.valueOf(a, 2);
-//////        int oct = Integer.valueOf(a, 8);
-//////        int dec = Integer.valueOf(a, 10);
-//////        int Hex = Integer.valueOf(a, 16);
-//////      */
-//
-//////    public memory() {
-//////        /*
-//////        case input  M+ adds the current displayed value in the memory and updates display
-//////        case input  MC reset the memory
-//////        case MRC recall the current value from memory to the display
-//////         */
-//////    }
-////      public unitSwitch() {
-////////        /*
-////////        case radian Math.toRadians(num)
-////////        case degree Math.toDegree(num)
-////////         */
-////////    }
-////////    public logFunctions() {
-////////        /*
-////////        case log = Math.log10(number),number
-////////        case natural log = Math.log(number)
-////////        case inverse log = Math.exp(Math.log10())
-////////        case inverse natural log = Math.exp(Math.log())
-////////         */
-////////    }
+
+
 }
