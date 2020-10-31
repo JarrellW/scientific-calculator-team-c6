7c5dadd09fd732807916147a441e581cfa5d3d67
Jay <73120876+JarrellW@users.noreply.github.com>
2020-10-31 18:24:21 -0400

    added Calculator Methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -30,15 +30,17 @@ public class Console {
 
     }
 
-    public static Double getDoubleInput(String prompt) {
-        return null;
-    }
+   // public static Double getDoubleInput(String prompt) {
+       // return null;
+    //}
     public static void printMenuOptions() {
-        Console.println("1: add");
-        Console.println("2: subtract");
-        Console.println("3: multiply");
-//        Console.println("4: divide");
-//        Console.println("5: square");
-
+        Console.println("1:Add  2:Subtract  3:Multiply 4:Divide 5:Square 6:Square Root" +
+                " 7:Inverse 8:Switch Sign \n9:Factorial 10:Bonus Feature 11:Bonus Feature2 " +
+                "12:Trig Functions Menu 13:Switch Trig Units Menu \n14:Logarithmic Functions Menu  " +
+                "15:Switch Number Display Menu  16:Memory Settings Menu");
+    }
+    public static void printTrigFunctionsMenuOptions() {
+        Console.println("Tri Functions Menu: \n 1:Sine 2:Cosine 3:Tangent " +
+                "4:Inverse Sine \n5:Inverse Cosine 6:Inverse Tangent");
     }
 }
