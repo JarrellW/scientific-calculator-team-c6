e360f12959e90c32c761686c5e1547315416cb0e
TomCorson <corsontom@yahoo.com>
2020-11-01 18:03:02 -0500

    saving

---
+++ src/test/java/com/zipcodewilmington/scientificcalculator/SciFunctionsTest.java
@@ -0,0 +1,28 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SciFunctionsTest {
+    @Test
+    public void sin() {
+        Double num1 = 4.0;
+        Double expectedResult = -0.7568024953079282;
+        Double  actualResult = SciFunctions.sin(num1);
+        assertEquals(expectedResult, actualResult);
+    }
+    public void cos() {
+        Double num1 = 98.8;
+        Double expectedResult = -0.1594846391;
+        Double  actualResult = SciFunctions.cos(num1);
+        assertEquals(expectedResult, actualResult);
+    }
+    public void tan() {
+        Double num1 = 98.8;
+        Double expectedResult = -0.1594846391;
+        Double  actualResult = SciFunctions.cos(num1);
+        assertEquals(expectedResult, actualResult);
+    }
+
+}
