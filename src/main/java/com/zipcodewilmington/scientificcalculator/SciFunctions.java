7d0517a1afe2656b354edd9132d5a0f0a8b31f6c
kyle <kyle@zipcodes-MBP.fios-router.home>
2020-11-01 11:19:08 -0500

    added to the answer print out for trig and log

--- src/main/java/com/zipcodewilmington/scientificcalculator/SciFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciFunctions.java
@@ -5,65 +5,65 @@ public class SciFunctions {
     public static void sin() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, (Math.sin(i)));
+        Console.println("sin(%s) = %s", i, (Math.sin(i)));
 
     }
 
     public static void cos() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, (Math.cos(i)));
+        Console.println("cos(%s) = %s", i, (Math.cos(i)));
     }
 
     public static void tan() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, (Math.tan(i)));
+        Console.println("tan(%s) = %s", i, (Math.tan(i)));
     }
 
     public static void sec() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, 1 / (Math.cos(i)));
+        Console.println("sec(%s) = %s", i, 1 / (Math.cos(i)));
     }
 
     public static void csc() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, 1 / (Math.sin(i)));
+        Console.println("csc(%s) = %s", i, 1 / (Math.sin(i)));
     }
 
     public static void cot() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, 1 / (Math.tan(i)));
+        Console.println("cot(%s) = %s", i, 1 / (Math.tan(i)));
     }
 
     public static void log() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, Math.log10(i));
+        Console.println("log(%s) = %s", i, Math.log10(i));
 
     }
 
     public static void naturalLog() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, Math.log(i));
+        Console.println("ln(%s) = %s", i, Math.log(i));
 
     }
 
     public static void inverseLog() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, (Math.exp(Math.log10(i))));
+        Console.println("inverse log(%s) = %s", i, (Math.exp(Math.log10(i))));
 
     }
 
     public static void inverseNaturalLog() {
         double i = Console.getIntegerInput("Enter an integer");
         Console.println("The user input %s as a integer", i);
-        Console.println("%s = %s", i, (Math.exp(Math.log(i))));
+        Console.println("inverse ln(%s) = %s", i, (Math.exp(Math.log(i))));
     }
 }
     // Mode switching not sure how to use this yet understand changing the string formatting
