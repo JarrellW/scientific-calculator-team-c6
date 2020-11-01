c1a6d2cdbac74d6e9485af2dbcde4425821d9ab6
TomCorson <corsontom@yahoo.com>
2020-11-01 13:39:15 -0500

    edit Sci func to doubles simpify prompts

--- src/main/java/com/zipcodewilmington/scientificcalculator/SciFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciFunctions.java
@@ -3,66 +3,56 @@ import com.zipcodewilmington.scientificcalculator.Console;
 
 public class SciFunctions {
     public static void sin() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter number");
         Console.println("sin(%s) = %s", i, (Math.sin(i)));
 
     }
 
     public static void cos() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("cos(%s) = %s", i, (Math.cos(i)));
     }
 
     public static void tan() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("tan(%s) = %s", i, (Math.tan(i)));
     }
 
     public static void sec() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("sec(%s) = %s", i, 1 / (Math.cos(i)));
     }
 
     public static void csc() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("csc(%s) = %s", i, 1 / (Math.sin(i)));
     }
 
     public static void cot() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("cot(%s) = %s", i, 1 / (Math.tan(i)));
     }
 
     public static void log() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("log(%s) = %s", i, Math.log10(i));
 
     }
 
     public static void naturalLog() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("ln(%s) = %s", i, Math.log(i));
 
     }
 
     public static void inverseLog() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("inverse log(%s) = %s", i, (Math.exp(Math.log10(i))));
 
     }
 
     public static void inverseNaturalLog() {
-        double i = Console.getIntegerInput("Enter an integer");
-        Console.println("The user input %s as a integer", i);
+        Double i = Console.getDoubleInput("Enter a number");
         Console.println("inverse ln(%s) = %s", i, (Math.exp(Math.log(i))));
     }
 }
